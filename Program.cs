e6042b02b93982f1a82067be531310eb5176c736
Wrdouuu <164611150+Wrdouuu@users.noreply.github.com>
2024-08-16 20:47:53 +0700

    s

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace KIEMTRASONGUYENTO;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter your number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if(number < 2)
+            {
+                Console.WriteLine(number +"is not a prime number");
+            }
+            else
+            {
+                 int i = 2;
+                bool check = true;
+                while (i <= Math.Sqrt(number))
+                {
+                    if (number % i == 0)
+                    {
+                        check = false;
+                        break;
+                    }
+                    i++;
+                }
+                if (check)
+                {
+                    Console.WriteLine(number + " is a prime");
+                }
+                else
+                {
+                    Console.WriteLine(number + " is not a prime");
+            }
+        }
+    }
+}
